4f7376ac7e91bce07d758895896f34ae87246c7f
Baertram <pz1@gmx.net>
2025-08-16 16:00:30 +0200

    -Updated for current PTS API101047

--- LibZone/LibZone.lua
+++ LibZone/LibZone.lua
@@ -173,7 +173,7 @@ end
 --+ the current zone's index and parent zone index
 --> Returns: number currentZoneId, number currentZoneParentId, number currentZoneIndex, number currentZoneParentIndex, number mapId, number mapIndex, number parentMapId
 function lib:GetCurrentZoneIds()
-    local currentZoneIndex = GetCurrentMapZoneIndex()
+    local currentZoneIndex = GetUnitZoneIndex("player") --GetCurrentMapZoneIndex() -> this might return the currently opened worldmap's zoneIndex and thus be wrong!
     local currentZoneId = GetZoneId(currentZoneIndex)
     local currentZoneParentId = GetParentZoneId(currentZoneId)
     local currentZoneParentIndex = GetZoneIndex(currentZoneParentId)
